965f09d3a64c703b3eb1d4b3460aadc177a991d4
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-06-22 12:58:46 -0700

    profile page

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ function ensureAuthenticated(req, res, next) {
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
-  
+
   app.route('/').get((req, res) => {
     res.render('index', {
       title: "Connected to Database",
@@ -44,39 +44,41 @@ myDB(async client => {
   });
 
   app.route('/login').post(
-    passport.authenticate('local', { failureRedirect: '/' }), 
+    passport.authenticate('local', { failureRedirect: '/' }),
     (req, res) => {
       res.redirect('/profile');
     }
   );
-  
+
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render('profile');
-  });
-
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-  
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
-
-  passport.use(new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username: username }, (err, user) => {
-      console.log(`User ${username} attempted to log in.`);
-      if (err) return done(err);
-      if (!user) return done(null, false);
-      if (password !== user.password) return done(null, false);
-      return done(null, user);
+    res.render('profile', {
+      username: req.user.username
     })
-  }))
+  });
 
-}).catch(e => {
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, doc);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, done) => {
+  myDataBase.findOne({ username: username }, (err, user) => {
+    console.log(`User ${username} attempted to log in.`);
+    if (err) return done(err);
+    if (!user) return done(null, false);
+    if (password !== user.password) return done(null, false);
+    return done(null, user);
+  })
+}))
+
+}).catch (e => {
   app.route('/').get((req, res) => {
-    res.render('index', { title: e, message: 'unable to connect to database'});
+    res.render('index', { title: e, message: 'unable to connect to database' });
   })
 })
 
