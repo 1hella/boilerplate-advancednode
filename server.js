8aa2420ca91d13c123e87e38b594897383ae3bfd
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-06-22 18:33:24 -0700

    added register route

--- server.js
+++ server.js
@@ -63,28 +63,30 @@ myDB(async client => {
     })
   });
 
-  app.route('/register').post((req, res, next) => {
-    myDataBase.findOne({ username: req.body.username }, (err, user) => {
-      if (err) {
-        next(err);
-      } else if (user) {
-        res.redirect('/');
-      } else {
-        myDataBase.insertOne({
-          username: req.body.username,
-          password: req.body.password
-        }, (err, doc) => {
-          if (err) {
-            res.redirect('/');
-          } else {
-            // the inserted document is held within
-            // the ops property of the doc
-            next(null, doc.ops[0]);
-          }
-        })
-      }
-    })
-  }, passport.authenticate('local', { failureRedirect: '/' }),
+  app.route('/register').post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne({
+            username: req.body.username,
+            password: req.body.password
+          }, (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              // the inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
+            }
+          })
+        }
+      })
+    },
+    passport.authenticate('local', { failureRedirect: '/' }),
     (req, res, next) => {
       res.redirect('/profile');
     }
