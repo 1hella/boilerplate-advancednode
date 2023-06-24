1c5040eb291bd14ecaf4660597c9485531a53a77
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-06-23 20:01:25 -0700

    auth

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const { ObjectID } = require('mongodb')
 const LocalStrategy = require('passport-local');
-const GitHubStrategy = require('passport-github');
+const GitHubStrategy = require('passport-github').Strategy;
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 module.exports = function (app, myDataBase) {
@@ -27,11 +27,11 @@ module.exports = function (app, myDataBase) {
     passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "/auth/github/callback"
+        callbackURL: process.env.GITHUB_CALLBACK_URL
     },
         (accessToken, refreshToken, profile, done) => {
             myDatabase.findOne({ username: profile.id }, (err, user) => {
-                console.log(`User ${username} attempted to log in`);
+                console.log(profile);
                 if (err) return done(err);
                 if (!user) return done(null, false);
                 return done(null, user);
