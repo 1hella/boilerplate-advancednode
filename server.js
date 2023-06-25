c96aa5d1104a8b3f1dafdfdc73f8b420843af773
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-06-25 14:17:00 -0700

    config socket io authentication

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ const auth = require('./auth');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const passport = require('passport');
 const session = require('express-session');
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
 
 const app = express();
 const http = require('http').createServer(app)
 const io = require('socket.io')(http);
+
+const MONGO_URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: MONGO_URI});
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -20,12 +26,37 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data,  message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
@@ -35,12 +66,12 @@ myDB(async client => {
   let currentUsers = 0;
 
   io.on('connection', socket => {
-    console.log('a user has connected');
+    console.log(`user ${socket.request.user.username} connected`);
     currentUsers++;
     io.emit('user count', currentUsers);
     
     socket.on('disconnect', function () {
-      console.log('a user has disconnected');
+      console.log(`user ${socket.request.user.username} disconnected`);
       currentUsers--;
       io.emit('user count', currentUsers);
     });
